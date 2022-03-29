470c8be715e74ed1b8e4b843c9766b57db2e1ba5
Jehan Lucas Vieira <lucasarago.df@gmail.com>
2022-03-29 15:57:42 -0300

    Final

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,35 +5,35 @@ const data = [
   {
     id: "1",
     name: "Meu ICMS",
-    link: "https://github.com/brunowbbs/Frontend-III",
+    link: "https://www.enap.gov.br/pt/acontece/noticias/enap-e-ministerio-da-economia-divulgam-os-finalistas-da-primeira-fase-dos-desafios-de-acesso-a-credito-e-do-e-commerce",
     image:
       "https://diarural.com.br/wp-content/uploads/2020/11/154672-icms.png",
   },
   {
     id: "2",
     name: "Coletou",
-    link: "https://github.com/brunowbbs/Frontend-III",
+    link: "https://github.com/lucasriber01/Jogo_ambiental_Java_Swing-AWT",
     image:
       "https://vejasp.abril.com.br/wp-content/uploads/2016/12/reciclagem.png",
   },
   {
     id: "3",
     name: "Agenda 2030",
-    link: "https://github.com/brunowbbs/Frontend-III",
+    link: "https://github.com/lucasriber01/ONU",
     image:
       "https://ppgaquicultura.furg.br/images/Imagens/Agenda_2030/AGENDA_2030.png",
   },
   {
     id: "4",
     name: "Minha água",
-    link: "https://github.com/brunowbbs/Frontend-III",
+    link: "",
     image:
       "https://gorgulho.com/wp-content/uploads/2021/11/planeta-agua.jpg",
   },
   {
     id: "5",
     name: "Landing Page - Mama Mia",
-    link: "https://github.com/brunowbbs/Frontend-III",
+    link: "https://github.com/diegodecrescenzo/frontend2-checkpoint1",
     image:
       "https://static.baratocoletivo.com.br/2020/0810/oferta_15970830814459_destaque%201.jpg",
   },
